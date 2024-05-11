71663c727314795e0022ed611a50da976118263a
Wilton Silva <Wilton Silva>
2024-05-11 17:46:28 -0300

    Remove port config

--- app.py
+++ app.py
@@ -11,5 +11,6 @@ API_URL = os.environ.get('API_URL')
 def home():
     return 'Home Page Route'
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
 #    app.run(host='0.0.0.0', port=5001, debug=True)
