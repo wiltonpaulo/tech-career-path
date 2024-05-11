ff4e5d9596415e17ae28e33f76edbccbddb75af9
Wilton Silva <Wilton Silva>
2024-05-11 18:12:52 -0300

    replace app to index.py

---
+++ index.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for
+import requests
+import os
+
+app = Flask(__name__)
+
+# API endpoints
+API_URL = os.environ.get('API_URL')
+
+@app.route('/')
+def home():
+    return 'Home Page Route'
+
+# if __name__ == '__main__':
+# #    app.run(host='0.0.0.0', port=5001, debug=True)
